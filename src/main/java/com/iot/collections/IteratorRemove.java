dcb4b2f942be6e2f3e2ebe8e14b43aa0ae02ae31
Gangineni <gangineni@Ganginenis-MBP.lan>
2020-11-09 22:35:59 -0500

    Collections

---
+++ src/main/java/com/iot/collections/IteratorRemove.java
@@ -0,0 +1,38 @@
+package com.iot.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class IteratorRemove {
+	
+	/*
+	 * For value based operations use iterator and
+	 * 
+	 *  for index based operations use for loop
+	 * 
+	 */
+	
+	public static void main(String[] args) 
+	{
+		int valToDel = 40;
+		List<Integer> intArr = new ArrayList<Integer>();
+		
+		intArr.add(10);
+		intArr.add(20);
+		intArr.add(30);
+		intArr.add(40);
+		intArr.add(50);
+		
+		ListIterator<Integer> itr = intArr.listIterator();
+		
+		while(itr.hasNext())
+		{
+			if(itr.next()==valToDel)
+			{
+				itr.remove();
+			}
+		}
+		System.out.println(intArr);
+	}
+}
